61377987c4d4e74db633c31d36c099b0eccc2b2d
vasyaqwe <elekton.fafik@gmail.com>
2025-11-26 22:11:19 +0200

    fix formatting

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from "drizzle-kit";
+import { defineConfig } from "drizzle-kit"
 
-const BASE = "./src/database";
-const migrationsFolder = `${BASE}/migrations`;
+const BASE = "./src/database"
+const migrationsFolder = `${BASE}/migrations`
 
 export default defineConfig({
-  dialect: "postgresql",
-  schema: `${BASE}/schema.ts`,
-  out: migrationsFolder,
-  verbose: false,
-  migrations: { prefix: "timestamp" },
-});
+   dialect: "postgresql",
+   schema: `${BASE}/schema.ts`,
+   out: migrationsFolder,
+   verbose: false,
+   migrations: { prefix: "timestamp" },
+})
 
-export { migrationsFolder };
+export { migrationsFolder }
